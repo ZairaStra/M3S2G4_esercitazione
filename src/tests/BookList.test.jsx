5dc4188be10d3be21abd8c44960641a02e6c9145
ZairaStra <straticozaira@gmail.com>
2025-05-22 16:31:21 +0200

    Card number

---
+++ src/tests/BookList.test.jsx
@@ -0,0 +1,27 @@
+/* import { render, screen } from "@testing-library/react";
+import BookList from "../components/BookList";
+import fantasy from "../data/fantasy.json";
+
+describe("looks if cards number is the same of the fantasy.json's books", () => {
+  it("renders the correct number of cards", () => {
+    render(<BookList books={fantasy} />);
+    const cards = screen.getAllByTestId("singleBook");
+    expect(cards).toHaveLength(fantasy.length);
+  });
+});
+ */
+
+import { render, screen } from "@testing-library/react";
+import BookList from "../components/BookList";
+import { expect } from "vitest";
+import fantasy from "../data/fantasy.json";
+
+describe("BookList", () => {
+  it("renders as many Bootstrap cards as there are books", () => {
+    const { container } = render(<BookList books={fantasy} />);
+
+    const cards = container.querySelectorAll(".card");
+
+    expect(cards.length).toBe(fantasy.length); //  CONFRONTO
+  });
+});
